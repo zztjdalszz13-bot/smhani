98d3808b4bd6c0ab2f70c38793df94872a27b5a4
smhani <zztjdalszz13@gmail.com>
2025-11-24 12:42:45 +0900

    change logo and title

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
     };
 
     return (
-        <div className="login-page container">
+        <div className="login-container">
             <div className="login-card">
                 <h2 className="login-title">{t('auth.loginTitle')}</h2>
 
